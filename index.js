e653815818c5b1b0c7fde7e81377155d275d52fc
tathagat11 <83083161+tathagat11@users.noreply.github.com>
2023-06-14 15:43:32 +0000

    first ping

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
- 
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+		GatewayIntentBits.GuildMembers,
+  ]
+});
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}.`)
+})
+
+client.on('messageCreate', function(msg){
+    if(msg.content === 'ping'){
+        msg.reply('pong');
+    }
+});
+
+const token = process.env['TOKEN']
+client.login(token)
