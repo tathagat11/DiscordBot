50130c65b0a8830e58329d8f86c4dd255d3fb3ed
tathagat11 <83083161+tathagat11@users.noreply.github.com>
2023-06-15 11:26:43 +0000

    lots

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const { Configuration, OpenAIApi } = require ('openai');
+const { Configuration, OpenAIApi } = require('openai');
 const dotenv = require('dotenv');
+const fetch = require("node-fetch");
+const Database = require("@replit/database")
 
-dotenv.config();
+const db = new Database()
+
 
 const client = new Client({
   intents: [
@@ -17,11 +20,12 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}.`)
 })
 
+
 //ping command
 client.on('messageCreate', function(message) {
   if (message.author.bot) return;
-  if (message.content === 'ping') {
-    message.reply('pong');
+  if (message.content === 'hi') {
+    message.reply('hello there');
   }
 });
 //gay command
@@ -33,32 +37,90 @@ client.on('messageCreate', function(message) {
 });
 
 //ai bot
+dotenv.config();
 const openaikey = process.env['openai'];
 const openai = new OpenAIApi(new Configuration({
-    apiKey: openaikey,
-  })
+  apiKey: openaikey,
+})
 );
-client.on("messageCreate", async function (message) {
+client.on("messageCreate", async function(message) {
   if (message.author.bot) return;
-  
+  if((message.channelId == 1118587313325359215) || (message.channelId == 1118623267507421246)){
   try {
     const response = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: [
-            {role: "system", content: "You are a helpful assistant who responds succinctly"},
-            {role: "user", content: message.content}
-        ],
-      });
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: "system", content: "You are a helpful assistant who responds succinctly" },
+        { role: "user", content: message.content }
+      ],
+    });
 
     const content = response.data.choices[0].message;
     return message.reply(content);
 
   } catch (err) {
+    //console.log(err)
     return message.reply(
-      "As an AI robot, I errored out."
+      "Error."
     );
+  }}
+});
+
+//encouragement
+const sadWords = ["sad","depressed", "unhappy", "angry"]
+const starterEncouragements = ["Just don't be ", "Imagine being ", "NOOB! Don't be "]
+
+db.get("encouragements").then(encouragements => {
+  if(!encouragements || encouragements.length < 1){
+    db.set("encouragements",starterEncouragements)
+  }
+})
+//to add encourage message
+function udtEncouragements(eMessage){
+  db.get("encouragements").then(encouragements => {
+    encouragements.push([eMessage])
+    db.set("encouragements",encouragements)
+  })
+}
+
+//to delete encourage message
+function delEncouragements(index){
+  db.get("encouragements").then(encouragements => {
+    if(encouragements.length > index) {
+      encouragements.splice(index, 1)
+      db.set("encouragements",encouragements)
+    }
+  })
+}
+
+function getQuote() {
+  return fetch("https://zenquotes.io/api/random")
+    .then(res => {
+      return res.json()
+    })
+    .then(data => {
+      return data[0]["q"] + " - " + data[0]["a"]
+    })
+}
+
+client.on('messageCreate', function(message) {
+  if (message.author.bot) return;
+  if (message.content === '$inspire') {
+    getQuote().then(quote => message.reply(quote))
+  }
+  var word = '';
+  if (sadWords.some(word => message.content.includes(word))){
+    for(i in sadWords){
+      if(message.content.includes(sadWords[i]))
+        word = sadWords[i];
+    }
+    const encouragement = encouragements[Math.floor(Math.random()*encouragements.length)] +     word;
+    message.reply(encouragement);
   }
 });
 
 const token = process.env['TOKEN']
 client.login(token)
+
+
+
